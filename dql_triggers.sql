a9997ae5fd16e488e529b0ebbe68ef91f90594ab
WbeimarV <wbeimarvega@gmail.com>
2025-08-18 12:25:36 -0500

    feat: :sparkles: se añaden triggers

--- dql_triggers.sql
+++ dql_triggers.sql
@@ -98,3 +98,84 @@ BEGIN
     END IF;
 END;$$
 DELIMITER ;
+
+-- verificar stock para vender
+DELIMITER $$
+CREATE TRIGGER verificar_stock_antes_venta
+BEFORE INSERT ON detalle_venta
+FOR EACH ROW
+BEGIN
+    DECLARE existe INT DEFAULT 0;
+    DECLARE stock_actual DECIMAL(10,2) DEFAULT 0;
+
+    -- Verificar si el producto existe en inventario
+    SELECT COUNT(*) INTO existe
+    FROM inventario
+    WHERE id_producto = NEW.id_producto;
+
+    IF existe = 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'El producto no existe en el inventario.';
+    ELSE
+        -- Obtener la cantidad actual del inventario
+        SELECT cantidad_actual INTO stock_actual
+        FROM inventario
+        WHERE id_producto = NEW.id_producto;
+
+        -- Verificar si hay suficiente stock
+        IF stock_actual < NEW.cantidad THEN
+            SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'Inventario insuficiente para realizar la venta.';
+        END IF;
+    END IF;
+END$$
+DELIMITER ;
+
+-- actualizar estado maquina cuando entra a mantenimiento
+DELIMITER $$
+CREATE TRIGGER actualizar_estado_maquinaria_por_mantenimiento
+AFTER INSERT ON mantenimiento_maquinaria
+FOR EACH ROW
+BEGIN
+  update maquinaria
+  set estado = "En matenimiento"
+  where id_maquinaria = new.id_maquinaria;
+END$$
+DELIMITER ;
+
+-- actualizar estado maquina segun fecha fucionamiento
+DELIMITER $$
+CREATE TRIGGER actualizar_estado_maquinaria_funcionamiento
+AFTER INSERT ON actividad_maquinaria
+FOR EACH ROW
+BEGIN
+  DECLARE hoy DATE;
+  SET hoy = CURDATE();
+
+  IF hoy >= NEW.fecha_inicio AND hoy <= NEW.fecha_fin THEN
+    UPDATE maquinaria
+    SET estado = 'En funcionamiento'
+    WHERE id_maquinaria = NEW.id_maquinaria;
+  END IF;
+END$$
+DELIMITER ;
+
+
+-- impedir ventas de empleado inactivo
+DELIMITER $$
+CREATE TRIGGER bloquear_venta_empleado_inactivo
+BEFORE INSERT ON venta
+FOR EACH ROW
+BEGIN
+  DECLARE estado_empleado VARCHAR(20);
+
+  SELECT estado INTO estado_empleado
+  FROM empleado
+  WHERE id_empleado = NEW.id_empleado;
+
+  IF estado_empleado <> 'Activo' THEN
+    SIGNAL SQLSTATE '45000'
+      SET MESSAGE_TEXT = 'El empleado no se encuentra activo y no puede registrar ventas';
+  END IF;
+END$$
+DELIMITER ;
